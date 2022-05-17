b93758f5cbcc1c82dcdcfeed0c9a2b91dd9aa7cb
Praveen Kumar <praveen.42000@gmail.com>
2022-05-17 21:41:59 +0530

    first commit

---
+++ app/src/main/java/com/techunico/coromandelcoopsociety/MainActivity.java
@@ -0,0 +1,22 @@
+package com.techunico.coromandelcoopsociety;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://coromandelcoopsociety.com/index.php");
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+}
